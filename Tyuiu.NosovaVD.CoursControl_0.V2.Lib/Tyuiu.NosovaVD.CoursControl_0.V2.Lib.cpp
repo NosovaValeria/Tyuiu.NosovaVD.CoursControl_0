c742ee28e8607362b876bf5005ec578eacf51fdf
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-19 16:46:15 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.NosovaVD.CoursControl_0.V2.Lib/Tyuiu.NosovaVD.CoursControl_0.V2.Lib.cpp
@@ -0,0 +1,17 @@
+﻿// Tyuiu.NosovaVD.CoursControl_0.V2.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.CoursControl_0/Tyuiu.CoursControl_0.cpp"
+#include <math.h>
+
+// TODO: This is an example of a library function
+class Service : public ISprint0Task8V1
+{
+	virtual int Chislo(float a, float b) override
+	{
+		return sqrt((12 * a + pow(a, 4)) / (pow(a, 2) + sqrt(234 + b)));
+	}
+};
---
+++ Tyuiu.NosovaVD.CoursControl_0.V2.Test/Tyuiu.NosovaVD.CoursControl_0.V2.Test.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.CoursControl_0.V2.Lib/Tyuiu.NosovaVD.CoursControl_0.V2.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task8V1* service = new Service();
+			float a = 2;
+			float b = 22;
+			int c = service->Chislo(a, b);
+			Assert::AreEqual(1, c);
+		}
+	};
+}
