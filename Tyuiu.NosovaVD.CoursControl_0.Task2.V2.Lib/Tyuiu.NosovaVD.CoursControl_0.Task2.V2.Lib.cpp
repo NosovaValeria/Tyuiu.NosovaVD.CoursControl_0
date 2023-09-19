1741cda8c5deeb64a58ec535560357cfac911b3e
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-19 16:51:05 +0500

    Task2

---
+++ Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Lib/Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Lib.cpp
@@ -0,0 +1,18 @@
+﻿// Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.CoursControl_0/Tyuiu.CoursControl_0.cpp"
+#include <math.h>
+
+// TODO: This is an example of a library function
+class Service1 : public ISprint0Task8V2
+{
+	virtual int Rezultat(int a, int b, int c) override
+	{
+		int rez = (a + b - c) / (pow(a, 2) + 2);
+		return rez;
+	}
+};
---
+++ Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Test/Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Lib/Tyuiu.NosovaVD.CoursControl_0.Task2.V2.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task8V2* service1 = new Service1();
+			int a = 2;
+			int b = 15;
+			int c = 5;
+			int d = service1->Rezultat(a, b, c);
+			Assert::AreEqual(2, d);
+		}
+	};
+}
